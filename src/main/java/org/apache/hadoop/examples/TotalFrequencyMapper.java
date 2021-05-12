0843e32005fe6f1b061906cfab8e152417330aed
Remco <remco.taal@student.hu.nl>
2021-05-12 19:10:33 +0200

    TotalFrequencyMapper & Reducer

---
+++ src/main/java/org/apache/hadoop/examples/TotalFrequencyMapper.java
@@ -0,0 +1,24 @@
+package org.apache.hadoop.examples;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+
+    class TotalFrequencyMapper
+            extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+
+        public void map(LongWritable key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            String[] stringArray = value.toString().split(" |\t");
+            Text firstChar = new Text (stringArray[0].substring(0, 1));
+            IntWritable frequency = new IntWritable(Integer.parseInt(stringArray[stringArray.length -1].replaceAll("[^0-9]", "")));
+
+            context.write(firstChar, frequency);
+
+        }
+    }
